8648ae6f6576012a6142683d47d58cfe144f294d
Florin12er <142166195+Florin12er@users.noreply.github.com>
2023-12-28 16:25:46 +0100

    visiulasize the tree and added level order inorder preoder postorder and height

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 class Node {
-  constructor(data, left = null, right = null) {
+  constructor(data, left = null, right = null, parent = null) {
     this.data = data;
     this.left = left;
     this.right = right;
+    this.parent = parent;
   }
 }
+
 class BinarySearch {
   constructor() {
     this.root = null;
   }
+
   add(data) {
     const node = this.root;
     if (node === null) {
@@ -18,14 +21,14 @@ class BinarySearch {
       const searchTree = (node) => {
         if (data < node.data) {
           if (node.left === null) {
-            node.left = new Node(data);
+            node.left = new Node(data, null, null, node);
             return;
           } else if (node.right !== null) {
             return searchTree(node.left);
           }
         } else if (data > node.data) {
           if (node.right === null) {
-            node.right = new Node(data);
+            node.right = new Node(data, null, null, node);
             return;
           } else if (node.right !== null) {
             return searchTree(node.right);
@@ -83,6 +86,114 @@ class BinarySearch {
     }
     return false;
   }
+  levelOrder(callback) {
+    const result = [];
+    const queue = [];
+    if (this.root !== null) {
+      queue.push(this.root);
+      while (queue.length > 0) {
+        const node = queue.shift();
+        result.push(callback ? callback(node) : node.data);
+        if (node.left !== null) queue.push(node.left);
+        if (node.right !== null) queue.push(node.right);
+      }
+    }
+    return result;
+  }
+
+  inOrder(callback) {
+    const result = [];
+    const traverse = (node) => {
+      if (node !== null) {
+        traverse(node.left);
+        result.push(callback ? callback(node) : node.data);
+        traverse(node.right);
+      }
+    };
+    traverse(this.root);
+    return result;
+  }
+
+  preOrder(callback) {
+    const result = [];
+    const traverse = (node) => {
+      if (node !== null) {
+        result.push(callback ? callback(node) : node.data);
+        traverse(node.left);
+        traverse(node.right);
+      }
+    };
+    traverse(this.root);
+    return result;
+  }
+
+  postOrder(callback) {
+    const result = [];
+    const traverse = (node) => {
+      if (node !== null) {
+        traverse(node.left);
+        traverse(node.right);
+        result.push(callback ? callback(node) : node.data);
+      }
+    };
+    traverse(this.root);
+    return result;
+  }
+
+  height(node) {
+    if (node === null) return -1;
+    const leftHeight = this.height(node.left);
+    const rightHeight = this.height(node.right);
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  depth(node) {
+    if (node === null) return 0;
+    return this.depth(node.parent) + 1;
+  }
+
+  isBalanced() {
+    const checkBalance = (node) => {
+      if (node === null) return true;
+      const leftHeight = this.height(node.left);
+      const rightHeight = this.height(node.right);
+      if (
+        Math.abs(leftHeight - rightHeight) <= 1 &&
+        checkBalance(node.left) &&
+        checkBalance(node.right)
+      ) {
+        return true;
+      }
+      return false;
+    };
+    return checkBalance(this.root);
+  }
+
+  rebalance() {
+    const values = this.inOrder();
+    return this.buildTree(values);
+  }
+
+  buildTree(values) {
+    const buildBalanced = (arr, start, end) => {
+      if (start > end) return null;
+      const mid = Math.floor((start + end) / 2);
+      const node = new Node(arr[mid]);
+      node.left = buildBalanced(arr, start, mid - 1);
+      node.right = buildBalanced(arr, mid + 1, end);
+      return node;
+    };
+
+    return buildBalanced(values, 0, values.length - 1);
+  }
+  visualizeTree(node = this.root, prefix = "", isLeft = true) {
+    if (node !== null) {
+      console.log(prefix + (isLeft ? "├── " : "└── ") + node.data);
+      const newPrefix = prefix + (isLeft ? "│   " : "    ");
+      this.visualizeTree(node.left, newPrefix, true);
+      this.visualizeTree(node.right, newPrefix, false);
+    }
+  }
   remove(data) {
     const removeNode = (node, data) => {
       if (node == null) {
@@ -116,11 +227,35 @@ class BinarySearch {
     this.root = removeNode(this.root, data);
   }
 }
+
 const bst = new BinarySearch();
 bst.add(4);
 bst.add(5);
 bst.add(6);
-bst.add(2);
+bst.add(4);
 bst.add(7);
 bst.add(3);
-console.log("minimum value:", bst.findMin()); // Should output 3 console.log("Maximum value:", bst.findMax()); // Should output 7 bst.remove(2); console.log("Minimum value:", bst.findMin()); // Should outpaut 3 console.log("Maximum value:", bst.findMax()); // Should output 7 bst.remove(7); console.log("Minimum value:", bst.findMin()); // Should output 3 console.log("Maximum value:", bst.findMax()); // Should output 6 console.log("Is 4 present:", bst.isPresent(4)); // Should output true console.log("Is 7 present:", bst.isPresent(7)); // Should output false console.log("Is 2 present:", bst.isPresent(2)); // Should output false
+bst.add(1);
+
+console.log("Original Tree Visualization:");
+bst.visualizeTree();
+console.log("minimum value:", bst.findMin());
+console.log("maximum value:", bst.findMax());
+
+bst.remove(7);
+
+console.log("\nLevel Order:", bst.levelOrder());
+console.log("In Order:", bst.inOrder());
+console.log("Pre Order:", bst.preOrder());
+console.log("Post Order:", bst.postOrder());
+console.log("Height of the tree:", bst.height(bst.root));
+console.log("Depth of a node:", bst.depth(bst.root));
+console.log("Is the tree balanced?", bst.isBalanced());
+
+const balancedTree = new BinarySearch();
+balancedTree.root = bst.rebalance();
+console.log("\nRebalanced Tree Visualization:");
+balancedTree.visualizeTree();
+console.log("minimum value:", bst.findMin());
+console.log("maximum value:", bst.findMax());
+console.log("is 2 present:", bst.isPresent(2));
